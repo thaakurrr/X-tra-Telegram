e195b4ccc9f089b4a0acdbe8fb6ea26da656cfae
thaakurrr <60512268+thaakurrr@users.noreply.github.com>
2020-01-31 19:29:34 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -15,7 +15,7 @@ USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your
 USER_BOT_NO_WARN = ("[──▄█▀█▄─────────██ \n▄████████▄───▄▀█▄▄▄▄ \n██▀▼▼▼▼▼─▄▀──█▄▄ \n█████▄▲▲▲─▄▄▄▀───▀▄ \n██████▀▀▀▀─▀────────▀▀](tg://user?id=742506768)\n\n"
                     "`Hello, this is X-tra-Telegram Security Service.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
-                    "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years.`\n\n"
+                    "Leave your name, phone number, address and 10k$ and hopefully you'll get a reply within 2 light years or msg me here @Thaakurr_bot .`\n\n"
                     "** Send** `/start` ** so that we can decide why you're here.**")
 
 
